748164b25b8932c71b8df0603dbdfc1bd0b121bc
Ariel Neumeyer <arielneu@outlook.com>
2021-03-13 10:37:50 -0300

    simple spring boot ws implementation

---
+++ src/main/java/com/neuyer/webservice/config/WebServiceConfig.java
@@ -0,0 +1,44 @@
+package com.neuyer.webservice.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter {
+
+    @Bean
+    //It is important to inject and set ApplicationContext to MessageDispatcherServlet. Without that, Spring WS will not automatically detect Spring beans.
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+        servlet.setTransformWsdlLocations(true);
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
+    }
+
+    @Bean(name = "countries")
+    //configures the annotation-driven Spring WS programming model. This makes it possible to use the various annotations, such as @Endpoint
+    //exposes a standard WSDL 1.1 by using XsdSchema
+    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("CountriesPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://neuyer.com/neuyerwebservice/web-service");
+        wsdl11Definition.setSchema(countriesSchema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema countriesSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("wsdl/countries.xsd"));
+    }
+}
---
+++ src/main/java/com/neuyer/webservice/ws/CountryEndpoint.java
@@ -0,0 +1,32 @@
+package com.neuyer.webservice.ws;
+
+import com.neuyer.neuyerwebservice.web_service.GetCountryRequest;
+import com.neuyer.neuyerwebservice.web_service.GetCountryResponse;
+import com.neuyer.webservice.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndpoint {
+    private static final String NAMESPACE_URI = "http://neuyer.com/neuyerwebservice/web-service";
+
+    private CountryRepository countryRepository;
+
+    //teria que ser colocado em uma service
+    @Autowired
+    public CountryEndpoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+
+        return response;
+    }
+}
